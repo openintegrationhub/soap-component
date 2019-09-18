3ef91e33d36b6f2ca13e4e7a48d90401159422e3
ElBabbler <gladikov.denis@gmail.com>
2019-09-18 12:13:40 +0300

    Adddign SOAP wrapper

--- src/main/java/io/elastic/soap/actions/SoapReplyAction.java
+++ src/main/java/io/elastic/soap/actions/SoapReplyAction.java
@@ -10,17 +10,21 @@ import io.elastic.api.Message;
 import io.elastic.api.Module;
 import io.elastic.soap.compilers.model.SoapBodyDescriptor;
 import io.elastic.soap.exceptions.ComponentException;
-import io.elastic.soap.handlers.RequestHandler;
 import io.elastic.soap.utils.Utils;
+import io.elastic.soap.validation.SOAPValidator;
+import io.elastic.soap.validation.impl.WsdlSOAPValidator;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.io.PipedInputStream;
-import java.io.PipedOutputStream;
 import javax.json.Json;
 import javax.json.JsonObject;
+import javax.xml.soap.MessageFactory;
+import javax.xml.soap.MimeHeaders;
 import javax.xml.soap.SOAPMessage;
+import org.json.JSONObject;
+import org.json.XML;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
 
 
 public class SoapReplyAction implements Module {
@@ -31,6 +35,8 @@ public class SoapReplyAction implements Module {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(SoapReplyAction.class);
 
+  private SOAPValidator validator;
+
   public static final String HEADER_CONTENT_TYPE = "Content-Type";
   public static final String HEADER_ROUTING_KEY = "X-EIO-Routing-Key";
   public static final String CONTENT_TYPE = "text/xml";
@@ -39,24 +45,35 @@ public class SoapReplyAction implements Module {
 
   @Override
   public void init(JsonObject configuration) {
-    LOGGER.info("On init started");
-    soapBodyDescriptor = loadClasses(configuration, soapBodyDescriptor);
-    LOGGER.info("On init finished");
+    try {
+      LOGGER.info("On init started");
+      soapBodyDescriptor = loadClasses(configuration, soapBodyDescriptor);
+      validator = new WsdlSOAPValidator(soapBodyDescriptor.getRequestBodyClassName());
+      LOGGER.info("On init finished");
+    } catch (ComponentException e) {
+      LOGGER.error("Error in init method", e);
+      throw e;
+    } catch (Exception e) {
+      LOGGER.error("Error in init method", e);
+      throw new ComponentException(e);
+    }
   }
 
 
   @Override
   public void execute(ExecutionParameters parameters) {
     try {
-      JsonObject headers = parameters.getMessage().getHeaders();
-      JsonObject inputBody = parameters.getMessage().getBody();
-      JsonObject configuration = parameters.getConfiguration();
-      Message inputMsg = parameters.getMessage();
+      final JsonObject headers = parameters.getMessage().getHeaders();
+      final JsonObject configuration = parameters.getConfiguration();
+      final Message inputMsg = parameters.getMessage();
+      final JsonObject body = Utils.getSoapBody(inputMsg.getBody());
+
       LOGGER.trace("Input configuration: {}", configuration);
       LOGGER.trace("Input headers: {}", headers);
-      LOGGER.trace("Input body: {}", inputBody);
+      LOGGER.trace("Input body: {}", body);
       if (VALIDATION_ENABLED.equals(configuration.getString(VALIDATION, VALIDATION_ENABLED))) {
-
+        LOGGER.trace("Validation is required for SOAP message");
+        validator.validate(body);
       }
       String replyTo = inputMsg.getHeaders().get("reply_to") != null ? inputMsg.getHeaders()
           .getString("reply_to") : null;
@@ -67,24 +84,20 @@ public class SoapReplyAction implements Module {
         LOGGER.error("No reply_to id found!");
         return;
       }
-//      final String xml = XML.toString(new JSONObject(body));
-//      final Document document = Utils.convertStringToXMLDocument(xml);
-//
-//      final SOAPMessage message = MessageFactory.newInstance().createMessage();
-//      final MimeHeaders soapHeaders = message.getMimeHeaders();
-//
-//      soapHeaders.addHeader("SOAPAction", soapBodyDescriptor.getSoapAction());
-//      message.getSOAPBody().addDocument(document);
+      final String xml = XML.toString(new JSONObject(body));
+      final Document document = Utils.convertStringToXMLDocument(xml);
+
+      final SOAPMessage message = MessageFactory.newInstance().createMessage();
+      final MimeHeaders soapHeaders = message.getMimeHeaders();
+
+      soapHeaders.addHeader("SOAPAction", soapBodyDescriptor.getSoapAction());
+      message.getSOAPBody().addDocument(document);
 
       LOGGER.info("Creating output message...");
-      String message = inputBody.toString();
-
-//      PipedInputStream in = new PipedInputStream();
-//      final PipedOutputStream outputStream = new PipedOutputStream(in);
-//      message.writeTo(outputStream);
+//      String message = body.toString();
 
       LOGGER.info("Building HTTP reply object...");
-      InputStream in = new ByteArrayInputStream(message.getBytes());
+      InputStream in = new ByteArrayInputStream(Utils.getStringOfSoapMessage(message).getBytes());
       HttpReply httpReply = new HttpReply.Builder()
           .content(in)
           .header(HEADER_ROUTING_KEY, replyTo)
@@ -94,11 +107,6 @@ public class SoapReplyAction implements Module {
 
       LOGGER.info("Making HTTP reply...");
       parameters.getEventEmitter().emitHttpReply(httpReply);
-
-      JsonObject body = Json.createObjectBuilder()
-//          .add("SoapResponse", Utils.getStringOfSoapMessage(message))
-          .add("SoapResponse", message)
-          .build();
 
       LOGGER.info("Emitting data...");
       parameters.getEventEmitter().emitData(new Message.Builder().body(body).build());
